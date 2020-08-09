f64758150c1649364da9d06f5e074e70af4ed1ff
hauravmandr <kumar@strongarmtech.com>
2020-08-10 01:51:41 +0530

    Updates

---
+++ precache-manifest.900de9983a7af8723dd764d0dfa40baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d3232500231a26516140457d2aa5c1",
+    "url": "/tooltip-cn/index.html"
+  },
+  {
+    "revision": "d9dcafd6a372af23d12e",
+    "url": "/tooltip-cn/static/css/main.4318b440.chunk.css"
+  },
+  {
+    "revision": "45d5362db39d30a7e975",
+    "url": "/tooltip-cn/static/js/2.67857723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tooltip-cn/static/js/2.67857723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9dcafd6a372af23d12e",
+    "url": "/tooltip-cn/static/js/main.1f76a704.chunk.js"
+  },
+  {
+    "revision": "2d952cfaf8efea17497c",
+    "url": "/tooltip-cn/static/js/runtime-main.b1bf6ad3.js"
+  }
+]);
